f85af8630f7305376f6ff54c91de1c1631ff1b59
hellgamer <19thanks96@gmail.com>
2023-11-12 19:28:01 +0200

    delete whitespaces

--- app.js
+++ app.js
@@ -11,213 +11,215 @@ var startRenderLoop = function (engine, canvas) {
 var engine = null;
 var scene = null;
 var sceneToRender = null;
-var createDefaultEngine = function() { return new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true,  disableWebGL2Support: false}); };
+var createDefaultEngine = function() { 
+    return new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true,  disableWebGL2Support: false}); 
+};
+
 var createScene = () => {
-const keys = {
-    jump: 0,
-    fly: 0,
-    letft: 0,
-    right: 0,
-    forward: 0,
-    back: 0
-}
-
-// create a basic BJS Scene object
-const scene = new BABYLON.Scene(engine)
-
-// scene.enablePhysics()
-scene.enablePhysics(null, new BABYLON.CannonJSPlugin())
-
-const camera = new BABYLON.ArcRotateCamera('arcCamera1', 0, 0, 10, BABYLON.Vector3.Zero(), scene)
-// camera.lowerRadiusLimit = camera.upperRadiusLimit = camera.radius;
-camera.attachControl(canvas, false)
-camera.setPosition(new BABYLON.Vector3(50, 100, 100))
-camera.checkCollisions = true
-camera.applyGravity = true
-
-camera.lowerRadiusLimit = 2
-camera.upperRadiusLimit = 20
-
-camera.keysLeft = []
-camera.keysRight = []
-camera.keysUp = []
-camera.keysDown = []
-
-BABYLON.SceneLoader.ImportMesh("", "./textures/soviet_house/", "scene.gltf", scene, function(newMeshes){
-    newMeshes[0].scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
-    newMeshes[0].position.y = -2
-    newMeshes[0].position.z = -2 
-    const house1 = newMeshes[0]
-    for(let i = 0; i < 10; i++) {
-        const house2 = house1.clone('house'+ i)
-        house2.position.y = -2
-        house2.position.z = 4*i
+    const keys = {
+        jump: 0,
+        fly: 0,
+        letft: 0,
+        right: 0,
+        forward: 0,
+        back: 0
     }
+
+    // create a basic BJS Scene object
+    const scene = new BABYLON.Scene(engine)
+
+    // scene.enablePhysics()
+    scene.enablePhysics(null, new BABYLON.CannonJSPlugin())
+
+    const camera = new BABYLON.ArcRotateCamera('arcCamera1', 0, 0, 10, BABYLON.Vector3.Zero(), scene)
+    // camera.lowerRadiusLimit = camera.upperRadiusLimit = camera.radius;
+    camera.attachControl(canvas, false)
+    camera.setPosition(new BABYLON.Vector3(50, 100, 100))
+    camera.checkCollisions = true
+    camera.applyGravity = true
+
+    camera.lowerRadiusLimit = 2
+    camera.upperRadiusLimit = 20
+
+    camera.keysLeft = []
+    camera.keysRight = []
+    camera.keysUp = []
+    camera.keysDown = []
+
+    BABYLON.SceneLoader.ImportMesh("", "./textures/soviet_house/", "scene.gltf", scene, function(newMeshes){
+        newMeshes[0].scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
+        newMeshes[0].position.y = -2
+        newMeshes[0].position.z = -2 
+        const house1 = newMeshes[0]
+        for(let i = 0; i < 10; i++) {
+            const house2 = house1.clone('house'+ i)
+            house2.position.y = -2
+            house2.position.z = 4*i
+        }
+        
+    }) 
+
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
     
-}) 
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
 
-let player;
+    let player;
 
-var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    BABYLON.SceneLoader.ImportMesh("", "", "./Baked_Animations_Intergalactic_Spaceships_Version_2/GLTF_EMBEDDED/Baked_Animations_Intergalactic_Spaceships_Version_2.gltf", scene, function(newMeshes){
+        newMeshes[0].scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
+        
+        player = newMeshes[0]
+        player.checkCollisions = true
 
-// Default intensity is 1. Let's dim the light a small amount
-light.intensity = 0.7;
+        player.physicsImpostor = new BABYLON.PhysicsImpostor(player, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1, friction: 100, restitution: 0.35 }, scene)
 
-BABYLON.SceneLoader.ImportMesh("", "", "./Baked_Animations_Intergalactic_Spaceships_Version_2/GLTF_EMBEDDED/Baked_Animations_Intergalactic_Spaceships_Version_2.gltf", scene, function(newMeshes){
-    newMeshes[0].scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
-    
-    player = newMeshes[0]
-    player.checkCollisions = true
+        const Material = new BABYLON.StandardMaterial('material', scene)
+        Material.emissiveColor = new BABYLON.Color3(0, 0.58, 0.86)
+        player.material = Material
+        camera.setTarget(player)
+        // camera.followMesh = player;
 
-    player.physicsImpostor = new BABYLON.PhysicsImpostor(player, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1, friction: 100, restitution: 0.35 }, scene)
+        let speed = 0.05
 
-    const Material = new BABYLON.StandardMaterial('material', scene)
-Material.emissiveColor = new BABYLON.Color3(0, 0.58, 0.86)
-player.material = Material
-camera.setTarget(player)
-// camera.followMesh = player;
+        player.update = function () {
+            var cameraForwardRayPosition = camera.getForwardRay().direction
+            var cameraForwardRayPositionWithoutY = new BABYLON.Vector3(cameraForwardRayPosition.x, 0, cameraForwardRayPosition.z)
 
-let speed = 0.05
+            if (keys) {
 
-player.update = function () {
-    var cameraForwardRayPosition = camera.getForwardRay().direction
-    var cameraForwardRayPositionWithoutY = new BABYLON.Vector3(cameraForwardRayPosition.x, 0, cameraForwardRayPosition.z)
+                if (keys.jump) {
+                    player.physicsImpostor.applyImpulse(new BABYLON.Vector3(0, 0.5, 0), player.getAbsolutePosition())
+                }
+                if (keys.fly) {
+                    player.physicsImpostor.applyImpulse(new BABYLON.Vector3(0, 0.17, 0), player.getAbsolutePosition())
+                    // player.lookAt(player.position.add(cameraForwardRayPositionWithoutX), 0, 0, 0);
+                    scene.getPhysicsEngine().setGravity(new BABYLON.Vector3(0, 0.05, 0))
+                    var v2 = BABYLON.Vector3.TransformCoordinates(new BABYLON.Vector3(0, 0.02, 0), BABYLON.Matrix.RotationY(player.rotation.y))
+                    player.position.addInPlace(v2)
+                }
+                if (keys.left) {
+                    // player.locallyTranslate(new BABYLON.Vector3(speed, 0, -speed))
+                    player.locallyTranslate(new BABYLON.Vector3(-speed, 0, 0))
+                }
+                if (keys.right) {
+                    // player.locallyTranslate(new BABYLON.Vector3(-speed, 0, -speed))
+                    player.locallyTranslate(new BABYLON.Vector3(speed, 0, 0))
+                }
+                if (keys.forward) {
+                    player.lookAt(player.position.add(cameraForwardRayPositionWithoutY), 0, 0, 0)
+                    player.position = player.position.add(new BABYLON.Vector3(cameraForwardRayPosition.x * speed, 0, cameraForwardRayPosition.z * speed))
+                    // var v2 = BABYLON.Vector3.TransformCoordinates(new BABYLON.Vector3(0, 0, -speed), BABYLON.Matrix.RotationY(player.rotation.y))
+                    // player.position.addInPlace(v2)
+                }
+                if (keys.back) {
+                    player.lookAt(player.position.add(cameraForwardRayPositionWithoutY), 0, 0, 0)
+                    player.position = player.position.add(new BABYLON.Vector3(-cameraForwardRayPosition.x * speed, 0, -cameraForwardRayPosition.z * speed))
+                }
+            }
+        }
+    });
 
-    if (keys) {
-    if (keys.jump) {
-        player.physicsImpostor.applyImpulse(new BABYLON.Vector3(0, 0.5, 0), player.getAbsolutePosition())
+    // var player2 = BABYLON.MeshBuilder.CreateCylinder("indicator", {
+    //    height: 1,
+    //   diameterTop: 0.5,
+    //   diameterBottom: 0
+    // }, scene);
+    // player2.checkCollisions = true;
+
+    // player.rotation.x = -Math.PI / 2
+    // player.bakeCurrentTransformIntoVertices()
+
+    engine.runRenderLoop(() => {
+        if (player != null) {
+            player.update()
+        }
+    })
+
+    window.addEventListener('keydown', handleKeyDown, false)
+    window.addEventListener('keyup', handleKeyUp, false)
+
+    let action = 16
+    function handleKeyDown (evt) {
+        if (evt.keyCode == 32) keys.jump = 1 // space
+        if (evt.keyCode == 70) keys.fly = 1 // f
+        // if (evt.keyCode == 81) keys.left = 1 // q
+        // if (evt.keyCode == 69) keys.right = 1 // e
+        if (evt.keyCode == 65 || evt.key == 'ArrowLeft') keys.left = 1 // A
+        if (evt.keyCode == 68 || evt.key == 'ArrowRight') keys.right = 1 // D
+        if (evt.keyCode == 87 || evt.key == 'ArrowUp') keys.forward = 1 // W
+        if (evt.keyCode == 83 || evt.key == 'ArrowDown') keys.back = 1 // S
+        if (evt.keyCode == 16) speed = 0.1 // shift
+
+        if (action !== evt.keyCode) {
+            action = evt.keyCode
+            console.log(action)
+        }
     }
-    if (keys.fly) {
-        player.physicsImpostor.applyImpulse(new BABYLON.Vector3(0, 0.17, 0), player.getAbsolutePosition())
-        // player.lookAt(player.position.add(cameraForwardRayPositionWithoutX), 0, 0, 0);
-        scene.getPhysicsEngine().setGravity(new BABYLON.Vector3(0, 0.05, 0))
-        var v2 = BABYLON.Vector3.TransformCoordinates(new BABYLON.Vector3(0, 0.02, 0), BABYLON.Matrix.RotationY(player.rotation.y))
-        player.position.addInPlace(v2)
-    }
-    if (keys.left) {
-        // player.locallyTranslate(new BABYLON.Vector3(speed, 0, -speed))
-        player.locallyTranslate(new BABYLON.Vector3(-speed, 0, 0))
-    }
-    if (keys.right) {
-        // player.locallyTranslate(new BABYLON.Vector3(-speed, 0, -speed))
-        player.locallyTranslate(new BABYLON.Vector3(speed, 0, 0))
-    }
-    if (keys.forward) {
-        player.lookAt(player.position.add(cameraForwardRayPositionWithoutY), 0, 0, 0)
-        player.position = player.position.add(new BABYLON.Vector3(cameraForwardRayPosition.x * speed, 0, cameraForwardRayPosition.z * speed))
-        // var v2 = BABYLON.Vector3.TransformCoordinates(new BABYLON.Vector3(0, 0, -speed), BABYLON.Matrix.RotationY(player.rotation.y))
-        // player.position.addInPlace(v2)
-    }
-    if (keys.back) {
-        player.lookAt(player.position.add(cameraForwardRayPositionWithoutY), 0, 0, 0)
-        player.position = player.position.add(new BABYLON.Vector3(-cameraForwardRayPosition.x * speed, 0, -cameraForwardRayPosition.z * speed))
-    }
-    }
-}
-});
+    function handleKeyUp (evt) {
+        if (evt.keyCode == 32) keys.jump = 0
+        if (evt.keyCode == 70) keys.fly = 0
+        // if (evt.keyCode == 81) keys.left = 0 // q
+        // if (evt.keyCode == 69) keys.right = 0 // e
+        if (evt.keyCode == 65 || evt.key == 'ArrowLeft') keys.left = 0
+        if (evt.keyCode == 68 || evt.key == 'ArrowRight') keys.right = 0
+        if (evt.keyCode == 87 || evt.key == 'ArrowUp') keys.forward = 0
+        if (evt.keyCode == 83 || evt.key == 'ArrowDown') keys.back = 0
+        if (evt.keyCode == 16) speed = 0.05
 
-
-
-// var player2 = BABYLON.MeshBuilder.CreateCylinder("indicator", {
-//    height: 1,
-//   diameterTop: 0.5,
-//   diameterBottom: 0
-// }, scene);
-// player2.checkCollisions = true;
-
-// player.rotation.x = -Math.PI / 2
-// player.bakeCurrentTransformIntoVertices()
-
-
-
-engine.runRenderLoop(() => {
-    if (player != null) {
-    player.update()
-    }
-})
-
-window.addEventListener('keydown', handleKeyDown, false)
-window.addEventListener('keyup', handleKeyUp, false)
-
-let action = 16
-function handleKeyDown (evt) {
-    if (evt.keyCode == 32) keys.jump = 1 // space
-    if (evt.keyCode == 70) keys.fly = 1 // f
-    // if (evt.keyCode == 81) keys.left = 1 // q
-    // if (evt.keyCode == 69) keys.right = 1 // e
-    if (evt.keyCode == 65 || evt.key == 'ArrowLeft') keys.left = 1 // A
-    if (evt.keyCode == 68 || evt.key == 'ArrowRight') keys.right = 1 // D
-    if (evt.keyCode == 87 || evt.key == 'ArrowUp') keys.forward = 1 // W
-    if (evt.keyCode == 83 || evt.key == 'ArrowDown') keys.back = 1 // S
-    if (evt.keyCode == 16) speed = 0.1 // shift
-
-    if (action !== evt.keyCode) {
         action = evt.keyCode
         console.log(action)
     }
-}
-function handleKeyUp (evt) {
-    if (evt.keyCode == 32) keys.jump = 0
-    if (evt.keyCode == 70) keys.fly = 0
-    // if (evt.keyCode == 81) keys.left = 0 // q
-    // if (evt.keyCode == 69) keys.right = 0 // e
-    if (evt.keyCode == 65 || evt.key == 'ArrowLeft') keys.left = 0
-    if (evt.keyCode == 68 || evt.key == 'ArrowRight') keys.right = 0
-    if (evt.keyCode == 87 || evt.key == 'ArrowUp') keys.forward = 0
-    if (evt.keyCode == 83 || evt.key == 'ArrowDown') keys.back = 0
-    if (evt.keyCode == 16) speed = 0.05
 
-    action = evt.keyCode
-    console.log(action)
-}
+    // Skybox
+    var skybox = BABYLON.Mesh.CreateBox('skyBox', 5000.0, scene)
+    var skyboxMaterial = new BABYLON.StandardMaterial('skyBox', scene)
+    skyboxMaterial.backFaceCulling = false
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture('./textures/TropicalSunnyDay', scene)
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0)
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0)
+    skyboxMaterial.disableLighting = true
+    skybox.material = skyboxMaterial
 
-// Skybox
-var skybox = BABYLON.Mesh.CreateBox('skyBox', 5000.0, scene)
-var skyboxMaterial = new BABYLON.StandardMaterial('skyBox', scene)
-skyboxMaterial.backFaceCulling = false
-skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture('./textures/TropicalSunnyDay', scene)
-skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE
-skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0)
-skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0)
-skyboxMaterial.disableLighting = true
-skybox.material = skyboxMaterial
+    // Ground
+    // const ground = BABYLON.MeshBuilder.CreateGround('ground', { width: 100, height: 100 })
+    // ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene)
 
-// Ground
-// const ground = BABYLON.MeshBuilder.CreateGround('ground', { width: 100, height: 100 })
-// ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene)
-
-var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "textures/heightMap.png", 100, 100, 100, 0, 10, scene, false, () => {
-    ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.HeightmapImpostor, { mass: 0, friction: 0.0, restitution: 0.7  })
-})
-ground.material = new BABYLON.GridMaterial('groundMaterial', scene)
-ground.position.y = -2
-ground.position.x = -28
-ground.checkCollisions = true
+    var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "textures/heightMap.png", 100, 100, 100, 0, 10, scene, false, () => {
+        ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.HeightmapImpostor, { mass: 0, friction: 0.0, restitution: 0.7  })
+    })
+    ground.material = new BABYLON.GridMaterial('groundMaterial', scene)
+    ground.position.y = -2
+    ground.position.x = -28
+    ground.checkCollisions = true
 
 
 
-// engine.runRenderLoop(() => { camera.alpha += 0.001; });
-return scene
+    // engine.runRenderLoop(() => { camera.alpha += 0.001; });
+    return scene
 }
 
-        window.initFunction = async function() {
-            
-            
-            
-            var asyncEngineCreation = async function() {
-                try {
-                return createDefaultEngine();
-                } catch(e) {
-                console.log("the available createEngine function failed. Creating the default engine instead");
-                return createDefaultEngine();
-                }
-            }
+window.initFunction = async function() {
 
-            window.engine = await asyncEngineCreation();
-if (!engine) throw 'engine should not be null.';
-startRenderLoop(engine, canvas);
-window.scene = createScene();};
-initFunction().then(() => {sceneToRender = scene                    
+    var asyncEngineCreation = async function() {
+        try {
+            return createDefaultEngine();
+        } catch(e) {
+            console.log("the available createEngine function failed. Creating the default engine instead");
+            return createDefaultEngine();
+        }
+    }
+
+    window.engine = await asyncEngineCreation();
+    if (!engine) throw 'engine should not be null.';
+
+    startRenderLoop(engine, canvas);
+    window.scene = createScene();
+};
+
+initFunction().then(() => {
+    sceneToRender = scene                    
 });
 
 // Resize
